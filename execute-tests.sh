7f96a0c1fed2f8d7a099ab2a209430434922edea
Joel Hari <joel@familiehari.ch>
2024-02-16 15:41:45 +0100

    update

--- execute-tests.sh
+++ execute-tests.sh
@@ -23,8 +23,8 @@ make all -j8 NR_DPUS=$1 USE_DPU=0 USE_MULTI_TASKLETS=1 PGFFT_DISABLE_SIMD=0
 
 echo -e ${txtcyn}Running Tests ...${txtwht}
 ./build/main > ../results/res-main-0-0-$1.txt
-./build/BGV_binary_arithmetic > ../results/res-binary-0-0-$1.txt
-./build/BGV_packed_arithmetic > ../results/res-packed-0-0-$1.txt
+# ./build/BGV_binary_arithmetic > ../results/res-binary-0-0-$1.txt
+# ./build/BGV_packed_arithmetic > ../results/res-packed-0-0-$1.txt
 ./build/BGV_country_db_lookup > ../results/res-country-0-0-$1.txt
 ./build/multiplications > ../results/res-multiplications-0-0-$1.txt
 
@@ -37,8 +37,8 @@ make all -j8 NR_DPUS=$1 USE_DPU=0 USE_MULTI_TASKLETS=1 PGFFT_DISABLE_SIMD=1
 
 echo -e ${txtcyn}Running Tests ...${txtwht}
 ./build/main > ../results/res-main-0-1-$1.txt
-./build/BGV_binary_arithmetic > ../results/res-binary-0-1-$1.txt
-./build/BGV_packed_arithmetic > ../results/res-packed-0-1-$1.txt
+# ./build/BGV_binary_arithmetic > ../results/res-binary-0-1-$1.txt
+# ./build/BGV_packed_arithmetic > ../results/res-packed-0-1-$1.txt
 ./build/BGV_country_db_lookup > ../results/res-country-0-1-$1.txt
 ./build/multiplications > ../results/res-multiplications-0-1-$1.txt
 
@@ -51,29 +51,17 @@ make clean
 echo -e ${txtcyn}Building HElib using DPUs ...${txtwht}
 make all -j8 NR_DPUS=$1 USE_DPU=1 USE_MULTI_TASKLETS=1 PGFFT_DISABLE_SIMD=1
 
-# ---- using 1 tasklet
-echo -e ${txtcyn}Building DPU programs using 1 DPU ...${txtwht}
+# ---- using 8 tasklets
+echo -e ${txtcyn}Building DPU programs using 8 DPU ...${txtwht}
 make clean-dpu
-make dpu -j8 NR_TASKLETS=1
+make dpu -j8 NR_TASKLETS=8
 
 echo -e ${txtcyn}Running Tests ...${txtwht}
-./build/main > ../results/res-main-1-1-$1.txt
-./build/BGV_binary_arithmetic > ../results/res-binary-1-1-$1.txt
-./build/BGV_packed_arithmetic > ../results/res-packed-1-1-$1.txt
-./build/BGV_country_db_lookup > ../results/res-country-1-1-$1.txt
-./build/multiplications > ../results/res-multiplications-1-1-$1.txt
-
-# ---- using 2 tasklets
-echo -e ${txtcyn}Building DPU programs using 2 DPU ...${txtwht}
-make clean-dpu
-make dpu -j8 NR_TASKLETS=2
-
-echo -e ${txtcyn}Running Tests ...${txtwht}
-./build/main > ../results/res-main-1-2-$1.txt
-./build/BGV_binary_arithmetic > ../results/res-binary-1-2-$1.txt
-./build/BGV_packed_arithmetic > ../results/res-packed-1-2-$1.txt
-./build/BGV_country_db_lookup > ../results/res-country-1-2-$1.txt
-./build/multiplications > ../results/res-multiplications-1-2-$1.txt
+./build/main > ../results/res-main-1-8-$1.txt
+# ./build/BGV_binary_arithmetic > ../results/res-binary-1-8-$1.txt
+# ./build/BGV_packed_arithmetic > ../results/res-packed-1-8-$1.txt
+./build/BGV_country_db_lookup > ../results/res-country-1-8-$1.txt
+./build/multiplications > ../results/res-multiplications-1-8-$1.txt
 
 # ---- using 4 tasklets
 echo -e ${txtcyn}Building DPU programs using 4 DPU ...${txtwht}
@@ -82,22 +70,34 @@ make dpu -j8 NR_TASKLETS=4
 
 echo -e ${txtcyn}Running Tests ...${txtwht}
 ./build/main > ../results/res-main-1-4-$1.txt
-./build/BGV_binary_arithmetic > ../results/res-binary-1-4-$1.txt
-./build/BGV_packed_arithmetic > ../results/res-packed-1-4-$1.txt
+# ./build/BGV_binary_arithmetic > ../results/res-binary-1-4-$1.txt
+# ./build/BGV_packed_arithmetic > ../results/res-packed-1-4-$1.txt
 ./build/BGV_country_db_lookup > ../results/res-country-1-4-$1.txt
 ./build/multiplications > ../results/res-multiplications-1-4-$1.txt
 
-# ---- using 8 tasklets
-echo -e ${txtcyn}Building DPU programs using 8 DPU ...${txtwht}
+# ---- using 2 tasklets
+echo -e ${txtcyn}Building DPU programs using 2 DPU ...${txtwht}
 make clean-dpu
-make dpu -j8 NR_TASKLETS=8
+make dpu -j8 NR_TASKLETS=2
 
 echo -e ${txtcyn}Running Tests ...${txtwht}
-./build/main > ../results/res-main-1-8-$1.txt
-./build/BGV_binary_arithmetic > ../results/res-binary-1-8-$1.txt
-./build/BGV_packed_arithmetic > ../results/res-packed-1-8-$1.txt
-./build/BGV_country_db_lookup > ../results/res-country-1-8-$1.txt
-./build/multiplications > ../results/res-multiplications-1-8-$1.txt
+./build/main > ../results/res-main-1-2-$1.txt
+# ./build/BGV_binary_arithmetic > ../results/res-binary-1-2-$1.txt
+# ./build/BGV_packed_arithmetic > ../results/res-packed-1-2-$1.txt
+./build/BGV_country_db_lookup > ../results/res-country-1-2-$1.txt
+./build/multiplications > ../results/res-multiplications-1-2-$1.txt
+
+# ---- using 1 tasklet
+echo -e ${txtcyn}Building DPU programs using 1 DPU ...${txtwht}
+make clean-dpu
+make dpu -j8 NR_TASKLETS=1
+
+echo -e ${txtcyn}Running Tests ...${txtwht}
+./build/main > ../results/res-main-1-1-$1.txt
+# ./build/BGV_binary_arithmetic > ../results/res-binary-1-1-$1.txt
+# ./build/BGV_packed_arithmetic > ../results/res-packed-1-1-$1.txt
+./build/BGV_country_db_lookup > ../results/res-country-1-1-$1.txt
+./build/multiplications > ../results/res-multiplications-1-1-$1.txt
 
 
 # -- using single-tasklet implementations
@@ -114,8 +114,8 @@ make dpu -j8 NR_TASKLETS=1
 
 echo -e ${txtcyn}Running Tests ...${txtwht}
 ./build/main > ../results/res-main-1-0-$1.txt
-./build/BGV_binary_arithmetic > ../results/res-binary-1-0-$1.txt
-./build/BGV_packed_arithmetic > ../results/res-packed-1-0-$1.txt
+# ./build/BGV_binary_arithmetic > ../results/res-binary-1-0-$1.txt
+# ./build/BGV_packed_arithmetic > ../results/res-packed-1-0-$1.txt
 ./build/BGV_country_db_lookup > ../results/res-country-1-0-$1.txt
 ./build/multiplications > ../results/res-multiplications-1-0-$1.txt
 
--- run-all.sh
+++ run-all.sh
@@ -2,8 +2,8 @@
 
 available_dpus=$1
 
-echo " ----- execute-tests ----------"
-./execute-tests.sh $available_dpus 2>&1 | tee execute-tests.log
-
 echo " ----- get-sys-info  ----------"
 ./get-sys-info.sh 2>&1 | tee get-sys-info.log
+
+echo " ----- execute-tests ----------"
+./execute-tests.sh $available_dpus 2>&1 | tee execute-tests.log
