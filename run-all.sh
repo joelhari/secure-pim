140cb8fbcec4db8f52c34b3902429e883e950f7b
Joel Hari <joel@familiehari.ch>
2024-05-04 10:38:57 +0200

    prepare for CModulus testing

--- run-all.sh
+++ run-all.sh
@@ -11,11 +11,11 @@ echo " ----- get-sys-info  ----------"
 # echo " ----- execute-add-tests ----------"
 # ./execute-add-tests.sh $available_dpus 2>&1 | tee execute-add-tests.log
 
-# echo " ----- execute-cmodulus-tests ----------"
-# ./execute-cmodulus-tests.sh $available_dpus 2>&1 | tee execute-cmodulus-tests.log
+echo " ----- execute-cmodulus-tests ----------"
+./execute-cmodulus-tests.sh $available_dpus 2>&1 | tee execute-cmodulus-tests.log
 
 # echo " ----- execute-ftt-layer-tests ----------"
 # ./execute-ftt-layer-tests.sh $available_dpus 2>&1 | tee execute-ftt-layer-tests.log
 
-echo " ----- execute-butterfly-loop-tests ----------"
-./execute-butterfly-loop-tests.sh $available_dpus 2>&1 | tee execute-butterfly-loop-tests.log
+# echo " ----- execute-butterfly-loop-tests ----------"
+# ./execute-butterfly-loop-tests.sh $available_dpus 2>&1 | tee execute-butterfly-loop-tests.log
